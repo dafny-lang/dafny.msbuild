1843f241c65e72ee98cffbb507af6e16eb5c945b
wouter <wouter@amazon.com>
2020-04-15 11:02:49 -0700

    attempt 2

--- Source/dafny.msbuild/DafnyCompileTask.cs
+++ Source/dafny.msbuild/DafnyCompileTask.cs
@@ -16,22 +16,44 @@ namespace DafnyMSBuild
      */
     public class DafnyCompileTask : Task
     {
-        private static string[] REQUIRED_PARAMS = { "timeLimit" };
-
         [Required]
         public string DafnyExecutable { get; set; }
         
         [Required]
         public ITaskItem[] DafnySourceFiles { get; set; }
 
+        [Required]
+        public string DafnyOutputFile { get; set; }
+
         public string Jobs { get; set; }
 
         [Required]
-        public String[] DafnyVerificationParams { get; set; }
+        public String[] DafnyCompileParams { get; set; } 
 
         public override bool Execute() {
-            Console.WriteLine("executed");
             return true;
+            // Determine all valid parameters
+            var verificationParamsDict = new Dictionary<string, string>();
+            foreach (var param in DafnyCompileParams)   
+            {
+                var keyVal = GetSplitParam(param);
+                verificationParamsDict[keyVal[0]] = keyVal.Length == 2 ? keyVal[1] : "";
+            }
+
+            // Determine if the verification task should be performed in parallel
+            ParallelOptions options = new ParallelOptions();
+            int jobCount;
+            if (Jobs != null && int.TryParse(Jobs, out jobCount))
+            {
+                options.MaxDegreeOfParallelism = jobCount;
+            }
+
+            // Verify all files
+            ConcurrentBag<bool> results = new ConcurrentBag<bool>();
+            Parallel.ForEach(DafnySourceFiles, options, file => {
+                results.Add(VerifyDafnyFile(file, verificationParamsDict));
+            });
+            return results.All(x => x);
         }
 
         private string[] GetSplitParam(string unsplitParam) {
@@ -47,7 +69,8 @@ namespace DafnyMSBuild
             
             using (Process verifyProcess = new Process()) {
                 verifyProcess.StartInfo.FileName = DafnyExecutable;
-                verifyProcess.StartInfo.ArgumentList.Add("/compile:0");
+                verifyProcess.StartInfo.ArgumentList.Add(
+                       String.Format("/out:{0} /compile:0 /spillTargetCode:3 /noVerify",DafnyOutputFile));
                 // Apply all relevant parameters
                 foreach (var entry in verificationParams) {
                     if (String.IsNullOrEmpty(entry.Value)) {
@@ -66,7 +89,7 @@ namespace DafnyMSBuild
                 verifyProcess.WaitForExit();
                 bool success = verifyProcess.ExitCode == 0;
                 
-                Log.LogMessage(MessageImportance.High, "Verifying {0} {1}", file.ItemSpec, success ? "succeeded!" : "failed:");
+                Log.LogMessage(MessageImportance.High, "Compiling {0} {1}", file.ItemSpec, success ? "succeeded!" : "failed:");
                 if (!success) {
                     string output = verifyProcess.StandardOutput.ReadToEnd();
                     Log.LogMessage(MessageImportance.High, output);
