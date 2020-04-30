dd8d047d4546c950de6c71efb30fb774fefde7a6
WouterSchols <wouter.schols@hotmail.com>
2020-04-30 13:20:11 -0700

    Simple Compile Pass-through (#15)
    * Passtrough Simple Addition
    
    * Added test case compilePasstrough
    
    * Replace tabs with spaces
    
    Co-authored-by: wouter <wouter@amazon.com>
    Co-authored-by: Robin Salkeld <salkeldr@amazon.com>

---
+++ Test/TestProject/CompilePasstroughUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Xunit;
+
+namespace CompilePasstroughTest
+{
+    public class CompilePasstroughUnitTest
+    {
+        [Fact]
+        public void CreatesLegendFile()
+        {
+            var legendFile =
+                System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
+                    "legend.txt");
+            Assert.True(File.Exists(legendFile), legendFile);
+        }
+    }
+}
