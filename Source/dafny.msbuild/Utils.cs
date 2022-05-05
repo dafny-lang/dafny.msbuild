9b2632399122cda1dc897a746867a6d16d55c44f
Alex Chew <alexchew@amazon.com>
2022-05-05 15:32:06 -0700

    code style

--- Source/dafny.msbuild/Utils.cs
+++ Source/dafny.msbuild/Utils.cs
@@ -32,7 +32,10 @@ namespace DafnyMSBuild
                     RedirectStandardOutput = true
                 }
             };
-            foreach (var arg in args) process.StartInfo.ArgumentList.Add(arg);
+            foreach (var arg in args)
+            {
+                process.StartInfo.ArgumentList.Add(arg);
+            }
             process.Start();
 
             // We must read before waiting, or else deadlock is possible.
